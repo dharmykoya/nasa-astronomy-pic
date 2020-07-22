e27bcdb2eaee337b76582efe74dec6a84d98ec60
Damilola Adekoya <dharmykoya38@gmail.com>
2020-07-22 23:08:45 +0100

    chore(setup-redux):setting up redux to manage state in the store

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./views/homePage/HomePage";
+import { setupStore } from "./store/reducer";
+import { Provider } from "react-redux";
+
+const store = setupStore();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Nasa Astronomy Pic</header>
-      <HomePage />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">Nasa Astronomy Pic</header>
+        <HomePage />
+      </div>
+    </Provider>
   );
 }
 
