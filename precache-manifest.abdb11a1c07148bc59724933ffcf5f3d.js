941d97bc12c1cbc22345f592a1ae4985f7a176d3
Damilola Adekoya <dharmykoya38@gmail.com>
2020-07-23 04:26:25 +0100

    Updates

---
+++ precache-manifest.abdb11a1c07148bc59724933ffcf5f3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b05be436b6ff248b9363e5b159a03b",
+    "url": "/nasa-astronomy-pic/index.html"
+  },
+  {
+    "revision": "73846bf06387341c967f",
+    "url": "/nasa-astronomy-pic/static/css/main.35bc2abd.chunk.css"
+  },
+  {
+    "revision": "0cc4c07b94ceaf81fa1f",
+    "url": "/nasa-astronomy-pic/static/js/2.d4234dff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nasa-astronomy-pic/static/js/2.d4234dff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73846bf06387341c967f",
+    "url": "/nasa-astronomy-pic/static/js/main.45e42407.chunk.js"
+  },
+  {
+    "revision": "9e80f7e705307b70fd29",
+    "url": "/nasa-astronomy-pic/static/js/runtime-main.bbacc36c.js"
+  }
+]);
